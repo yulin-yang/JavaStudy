b83698ade7186a18ad47ceeb7c59a92fd5a37a43
Nickdesk <1262022832@qq.com>
2024-10-06 16:23:42 +0800

    10.06

--- code/src/main/java/com/yang/dataStructure/MyTest.java
+++ code/src/main/java/com/yang/dataStructure/MyTest.java
@@ -1,46 +1,45 @@
 package com.yang.dataStructure;
+import org.junit.experimental.max.MaxCore;
 import sun.reflect.generics.tree.Tree;
 
 import java.util.*;
 
-class TreeNode {
-      int val;
-      TreeNode left;
-      TreeNode right;
-      TreeNode() {}
-      TreeNode(int val) { this.val = val; }
-      TreeNode(int val, TreeNode left, TreeNode right) {
-          this.val = val;
-          this.left = left;
-          this.right = right;
-      }
-  }
-
 class Solution {
-
-    private void proOrder(TreeNode root, List<Integer> res) {
-        if(root == null) {
-            return;
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        int ans = 0;
+        if(n == 0 || n == 1) {
+            return ans;
         }
 
-        proOrder(root.left, res);
-        res.add(root.val);
-        proOrder(root.right, res);
-    }
+        if(prices[1] > prices[0]) {
+            ans -= prices[0];
+        }
 
-    public List<Integer> preorderTraversal(TreeNode root) {
+        for(int i = 1; i < n - 1; i++) {
 
-        List<Integer> res = new ArrayList<>();
-        proOrder(root, res);
-        return res;
+            //假如当天的价格是 前一天、当天、后一天 价格中最低的，可以买入
+            if(prices[i] <= prices[i - 1] && prices[i] <= prices[i + 1]) {
+                ans -= prices[i];
+            }
+
+            //假如当天的价格是 前一天、当天、后一天 价格中最高的，可以卖出
+            if(prices[i] >= prices[i - 1]) {
+                ans += prices[i];
+            }
+
+
+        }
+        return ans;
+
     }
 }
-
-
 
 public class MyTest {
 
     public static void main(String[] args) {
+
+        System.out.println(new Solution().maxProfit(new int[]{1,2,3,4,5}));
 
 
     }
