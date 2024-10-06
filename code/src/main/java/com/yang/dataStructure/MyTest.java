7574929b199e59720c6e134e307c8e91e84b2aaa
Nickdesk <1262022832@qq.com>
2024-10-06 17:54:51 +0800

    10.06回家

--- code/src/main/java/com/yang/dataStructure/MyTest.java
+++ code/src/main/java/com/yang/dataStructure/MyTest.java
@@ -6,29 +6,14 @@ import java.util.*;
 
 class Solution {
     public int maxProfit(int[] prices) {
-        int n = prices.length;
+
         int ans = 0;
-        if(n == 0 || n == 1) {
-            return ans;
-        }
+        int n = prices.length;
 
-        if(prices[1] > prices[0]) {
-            ans -= prices[0];
-        }
-
-        for(int i = 1; i < n - 1; i++) {
-
-            //假如当天的价格是 前一天、当天、后一天 价格中最低的，可以买入
-            if(prices[i] <= prices[i - 1] && prices[i] <= prices[i + 1]) {
-                ans -= prices[i];
+        for(int i = 1; i < n; i++) {
+            if(prices[i] - prices[i - 1] > 0) {
+                ans += prices[i] - prices[i - 1];
             }
-
-            //假如当天的价格是 前一天、当天、后一天 价格中最高的，可以卖出
-            if(prices[i] >= prices[i - 1]) {
-                ans += prices[i];
-            }
-
-
         }
         return ans;
 
